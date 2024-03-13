aa0dd0d92670d6f651dcb6bfcb141c17276af877
BellaMacc <159152628+BellaMacc@users.noreply.github.com>
2024-03-13 11:07:36 -0400

    adding custom user
    Edited:
    website/models.py
               -added AUTH_USER_MODEL
    hungryhomies_site/settings.py
              -added CustomUser model
              -added oneToOne foregin key relationship to model.Posts with CustomeUser

--- hungryhomies_site/website/models.py
+++ hungryhomies_site/website/models.py
@@ -1,10 +1,25 @@
 # model definitions for datebase
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    user_birthday = models.DateTimeField()
+    user_program = models.CharField(max_length=50)
+    user_program = models.CharField(max_length=50)
+    user_bio = models.CharField(max_length=250)
+    user_allergies = models.CharField(max_length=500)
+    user_fav_food = models.CharField(max_length=100)
+    user_cmfrt_food = models.CharField(max_length=100)
+
+    #event_posts stuff
+
+
 class Post(models.Model):
-    account = None
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=750)
     location = models.CharField(max_length=200)
